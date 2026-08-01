40b04a530decb157562edb4441491147c48c6f41
Luis Blanco <luisblanco1337@gmail.com>
2026-08-01 16:36:56 +0400

    Fix installed deps-opengl rpath

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 import { install } from '@node-3d/addon-tools';
 
 const prefix = 'https://github.com/node-3d/glfw/releases/download';
-const tag = '7.1.0';
+const tag = '7.1.1';
 
 await install(`${prefix}/${tag}`);
