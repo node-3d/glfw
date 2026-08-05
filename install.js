f5a91782a93be231b88f98bb2aa82dac03d5b83e
Luis Blanco <luisblanco1337@gmail.com>
2026-08-06 02:50:37 +0400

    chore: prepare glfw 7.1.2

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 import { install } from '@node-3d/addon-tools';
 
 const prefix = 'https://github.com/node-3d/glfw/releases/download';
-const tag = '7.1.1';
+const tag = '7.1.2';
 
 await install(`${prefix}/${tag}`);
